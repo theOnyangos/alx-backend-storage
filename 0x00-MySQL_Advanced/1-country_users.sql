1029dca634e5468c57eb48f6f73b03743e043c61
theOnynagos <otienodennis29@gmail.com>
2023-07-13 19:08:15 +0300

    Create Users Table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- A SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email varchar(255) NOT NULL UNIQUE,
+  name varchar(255),
+  country ENUM('US','CO','TN') NOT NULL
+);
