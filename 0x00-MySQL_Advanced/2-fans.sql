03b0bfd6f0c0104387c3de0f19894a9ff073bb86
theOnynagos <otienodennis29@gmail.com>
2023-07-13 19:10:34 +0300

    Best Band Ever

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) as nb_fans  from metal_bands
+GROUP BY origin
+Order By nb_fans DESC;
